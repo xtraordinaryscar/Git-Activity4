3209c2655282292c3a4ea4b25db9434f9656dbd0
GeloGoLoko7 <gelomacchiato777@gmail.com>
2026-05-22 23:45:51 +0800

    Add thank_you function to activity.py

--- activity.py
+++ activity.py
@@ -2,10 +2,12 @@
 def greet(name):
     print(f"Hello, {name}! Welcome to our project!!!")
 
+def thankyou(name):
+    print(f"Thank you, {name}, kiss mo ko baby")
 
 
 
 #main
 name = input("Enter name: ")
-
 greet(name)
+thankyou(name)
